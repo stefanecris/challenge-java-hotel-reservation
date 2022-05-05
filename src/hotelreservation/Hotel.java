ec3e5589c63b4769a2e73c7a3237886ea361a87a
Stefane Oliveira <cristinastefane.38@gmail.com>
2022-05-05 16:00:36 -0300

    Add class Hotel

---
+++ src/hotelreservation/Hotel.java
@@ -0,0 +1,47 @@
+package hotelreservation;
+
+public class Hotel {
+    private String name;
+    private int rating;
+    private float weekdayRate, weekendRate;
+
+    public Hotel(String name, int rating, float weekdayRate, float weekendRate) {
+        this.name = name;
+        this.rating = rating;
+        this.weekdayRate = weekdayRate;
+        this.weekendRate = weekendRate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public float getWeekdayRate() {
+        return weekdayRate;
+    }
+
+    public void setWeekdayRate(float weekdayRate) {
+        this.weekdayRate = weekdayRate;
+    }
+
+    public float getWeekendRate() {
+        return weekendRate;
+    }
+
+    public void setWeekendRate(float weekendRate) {
+        this.weekendRate = weekendRate;
+    }
+    
+}
